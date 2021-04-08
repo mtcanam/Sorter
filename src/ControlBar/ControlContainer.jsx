91d6545028bd3d5e4752bac047fbd7533ed9b07c
Michael Canam <michael.canam@gmail.com>
2021-04-08 20:40:07 -0230

    Added the various sorting buttons.

--- src/ControlBar/ControlContainer.jsx
+++ src/ControlBar/ControlContainer.jsx
@@ -1,11 +1,16 @@
 import ResetButton from './ResetButton.jsx';
 import SortButton from './SortButton.jsx';
+import TestButton from './TestButton.jsx';
 
 export default function ControlContainer( props ) {
   return (
     <div className='control-container'>
       <ResetButton barCount={props.barCount} resetBars={props.resetBars} />
-      <SortButton sortBars={props.sortBars} />
+      <SortButton sortBars={props.mergeSort} text='Merge Sort' />
+      <SortButton sortBars={props.quickSort} text='Quick Sort' />
+      <SortButton sortBars={props.bubbleSort} text='Bubble Sort' />
+      <SortButton sortBars={props.heapSort} text='Heap Sort' />
+      <TestButton testSort={props.testSort} />
     </div>
   )
 }
